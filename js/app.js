25a07028713b7c4207da06c4f012b026ec1d74eb
simpleprop <nikgurov06@gmail.com>
2021-01-06 18:51:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,3 +8,11 @@ $('.spoilerButton2').on('click', function () {
   $('.spoilerButton2').slideUp();
   $('.spoiler2').slideDown(400);
 });
+$('.spoiler2').on('click', function () {
+  $('.spoiler2').slideUp();
+  $('.spoilerButton2').slideDown(400);
+});
+$('.spoiler').on('click', function () {
+  $('.spoiler').slideUp();
+  $('.spoilerButton').slideDown(400);
+});
